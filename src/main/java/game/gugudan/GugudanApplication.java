2a01b2cb733e0654b16ec7803351dfe5c6bfa06d
isu-nice <isu010614@naver.com>
2024-08-12 15:43:54 +0900

    init: init file

---
+++ src/main/java/game/gugudan/GugudanApplication.java
@@ -0,0 +1,13 @@
+package game.gugudan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GugudanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GugudanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/game/gugudan/GugudanApplicationTests.java
@@ -0,0 +1,13 @@
+package game.gugudan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GugudanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
